e63481b54190b8390b4e968497386b027dbb7cc3
Taufiq095 <117509527+Taufiq095@users.noreply.github.com>
2025-08-04 00:06:52 +0800

    Edit

--- src/jmtaufpracticum/Receiver.java
+++ src/jmtaufpracticum/Receiver.java
@@ -4,6 +4,35 @@ import java.util.ArrayList;
 
 public class Receiver {
     //Represents EmployeeManager, contains business logic
+    private ArrayList<Employee> employeeList;
 
-    ArrayList list<employee>
+    //constructor
+    public Receiver() {
+        employeeList = new ArrayList<>();
+    }
+
+    //Add employee, info, email address etc..
+    public void addEmployee(Employee emp) {
+        employeeList.add(emp);
+    }
+    //Delete Employee
+    public void deleteEmployee(int id, Employee emp) {
+        employeeList.remove(emp);
+    }
+
+
+    //update
+    public void updateEmployee(int id, Employee emp) {
+        employeeList.set(employeeList.indexOf(emp), emp);
+    }
+
+    //list
+
+
+    //find
+
+    //storeToFile
+    public void storeToFile(){
+
+    }
 }
